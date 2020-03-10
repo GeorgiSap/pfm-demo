cb6bd813c674af1f378033dd2631331408702f16
Georgi Sapundzhiev <georgisapundzhiev@Georgis-MacBook-Pro.local>
2020-03-10 02:07:55 +0200

    Added todos for tests

---
+++ src/test/java/com/example/pfmdemo/service/PortfolioManagementServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.pfmdemo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PortfolioManagementServiceTest {
+
+    @Test
+    void calculateAssetTimeSeries() {
+        //TODO implement
+    }
+}
---
+++ src/test/java/com/example/pfmdemo/web/rest/PortfolioManagementResourceTest.java
@@ -0,0 +1,13 @@
+package com.example.pfmdemo.web.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PortfolioManagementResourceTest {
+
+    @Test
+    void calculateAssetTimeSeries() {
+        //TODO implement
+    }
+}
